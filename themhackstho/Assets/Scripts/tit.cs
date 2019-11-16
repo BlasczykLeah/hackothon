eb57b9b548faad1eb4b2616aa38216eedbec3620
WesMcW <wesmcwhorter@gmail.com>
2019-11-16 03:47:27 -0600

    DONE

---
+++ themhackstho/Assets/Scripts/tit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tit : MonoBehaviour
+{
+    public GameObject howToPlay;
+
+    void Start()
+    {
+        howToPlay.SetActive(false);
+    }
+
+    public void openTit()
+    {
+        howToPlay.SetActive(true);
+    }
+}
