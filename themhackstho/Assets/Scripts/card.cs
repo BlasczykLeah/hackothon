5ba9e65b935f7850298230cbd494ae9a55ec4697
Lebrra <Lebrra@users.noreply.github.com>
2019-11-15 19:04:37 -0600

    card types

---
+++ themhackstho/Assets/Scripts/card.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class card : MonoBehaviour
+{
+    public int cardType;
+}
+
+public enum CardType
+{
+    Wizard, Knight, Archer, Sovereign, Dargon;
+}
--- themhackstho/Assets/Scripts/myDeck.cs
+++ themhackstho/Assets/Scripts/myDeck.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class myDeck : MonoBehaviour
 {
+    public List<Image> currentDeck;
+    public List<Image> myHand;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentDeck = new List<Image>();
+        myHand = new List<Image>();
+
     }
 
     // Update is called once per frame
